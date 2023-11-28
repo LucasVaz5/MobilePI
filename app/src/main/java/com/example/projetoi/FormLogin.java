947aa0d14f964df248c08e33b8f638032c8c80fc
LucasVaz5 <lucas.vs.br@gmail.com>
2023-11-28 16:07:48 -0300

    8 atualização do projeto

--- app/src/main/java/com/example/projetoi/FormLogin.java
+++ app/src/main/java/com/example/projetoi/FormLogin.java
@@ -1,36 +1,31 @@
 package com.example.projetoi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class FormLogin extends AppCompatActivity {
 
-    private TextView text_tela_cadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_login);
 
-        getSupportActionBar().hide();
+        AppCompatButton btn = findViewById(R.id.bt_criar_conta);
 
-        text_tela_cadastro.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent intent = new Intent(FormLogin.this, FormCadastro.class);
-                startActivity(intent);
-
-            }
+        btn.setOnClickListener(view -> {
+            startActivity(new Intent(FormLogin.this, FormCadastro.class));
         });
-    }
 
-    private void IniciarComponentes(){
-        text_tela_cadastro = findViewById(R.id.text_tela_cadastro);
     }
-
 }
+
+
+
+
