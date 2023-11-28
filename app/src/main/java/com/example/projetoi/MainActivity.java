0af4f41e63007fd5a2a36ba43fb633053716ada4
LucasVaz5 <lucas.vs.br@gmail.com>
2023-11-28 08:06:47 -0300

    Versao 3 do projeto

--- app/src/main/java/com/example/projetoi/MainActivity.java
+++ app/src/main/java/com/example/projetoi/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.projetoi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final Timer timer = new Timer();
+    TimerTask timerTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        gotoMainActivity();
+                    }
+                });
+            }
+        };
+        timer.schedule(timerTask,3000);
+    }
+
+    private void gotoMainActivity() {
+        Intent intent = new Intent(getApplicationContext(), FormLogin.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
     }
 }
